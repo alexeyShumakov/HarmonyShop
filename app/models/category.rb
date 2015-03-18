5afae1ae1fa1d0ddfa50e4d31adc7ca57e31c97e
bobshaftoe@yandex.ru <recpqq@gmail.com>
2015-03-18 06:11:14 +0500

    update get_category method

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,13 +2,6 @@ class Category < ActiveRecord::Base
   acts_as_nested_set
 
   def get_category
-    if root?
-      self_and_siblings
-    elsif leaf?
-      parent.self_and_siblings
-    else
-      self_and_siblings
-    end
+    root? || !leaf? ? self_and_siblings : parent.self_and_siblings
   end
-
 end
