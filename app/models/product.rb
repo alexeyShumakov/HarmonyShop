43953bb7cafb0f618babad4ef3ba906dba897896
bobshaftoe@yandex.ru <recpqq@gmail.com>
2015-03-22 14:37:54 +0500

    style and add to partial pagination

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
 	has_and_belongs_to_many :categories
-
+	default_scope ->{ order('updated_at') }
+	paginates_per 6
 	def assign_to_branch
 	categories.clear
 	@branch_nodes = Category.find(leaf_category_id).self_and_ancestors
