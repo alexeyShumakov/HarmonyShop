24291e92f19fc4c2832d8b21b1404a0509e03b1c
bobshaftoe@yandex.ru <recpqq@gmail.com>
2015-03-23 11:42:19 +0500

    commit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,6 +11,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+
     if cookies[:page_size]
       @cat_products = @category.products.page(params[:page]).per(cookies[:page_size])
     else
