1b9970eb977cdaa39a6a3dcabb23996bda10bbb2
alexeyShumakov <bobshaftoe@yandex.ru>
2015-04-03 05:31:33 +0500

    some fix

--- app/models/products_colors_size.rb
+++ app/models/products_colors_size.rb
@@ -3,4 +3,10 @@ class ProductsColorsSize < ActiveRecord::Base
   belongs_to :size
 
   validates :warehouse, numericality: {only_integer: true}
+
+  after_initialize :init
+
+  def init
+    self.warehouse  ||= 0
+  end
 end
