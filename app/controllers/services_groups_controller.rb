f0fc31d57bd8b187c4f7b0c4d83ca3b55f6d86b1
alexeyShumakov <bobshaftoe@yandex.ru>
2015-04-11 15:01:41 +0500

    update services group

--- app/controllers/services_groups_controller.rb
+++ app/controllers/services_groups_controller.rb
@@ -1,11 +1,13 @@
 class ServicesGroupsController < ApplicationController
-  before_action :current_user_admin?
+  before_filter :authenticate_user!
+  after_action :verify_authorized
   before_action :set_services_group, only: [:show, :edit, :update, :destroy]
 
   # GET /services_groups
   # GET /services_groups.json
   def index
     @services_groups = ServicesGroup.all
+    authorize ServicesGroup
   end
 
   # GET /services_groups/1
@@ -16,6 +18,7 @@ class ServicesGroupsController < ApplicationController
   # GET /services_groups/new
   def new
     @services_group = ServicesGroup.new
+    authorize @services_group
   end
 
   # GET /services_groups/1/edit
@@ -26,6 +29,7 @@ class ServicesGroupsController < ApplicationController
   # POST /services_groups.json
   def create
     @services_group = ServicesGroup.new(services_group_params)
+    authorize @services_group
 
     respond_to do |format|
       if @services_group.save
@@ -41,6 +45,8 @@ class ServicesGroupsController < ApplicationController
   # PATCH/PUT /services_groups/1
   # PATCH/PUT /services_groups/1.json
   def update
+    authorize @services_group
+
     respond_to do |format|
       if @services_group.update(services_group_params)
         format.html { redirect_to @services_group, notice: 'Services group was successfully updated.' }
@@ -55,6 +61,8 @@ class ServicesGroupsController < ApplicationController
   # DELETE /services_groups/1
   # DELETE /services_groups/1.json
   def destroy
+    authorize @services_group
+
     @services_group.destroy
     respond_to do |format|
       format.html { redirect_to services_groups_url, notice: 'Services group was successfully destroyed.' }
---
+++ spec/policies/services_group_policy_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe ServicesGroupPolicy do
+
+  let(:user) { User.new }
+
+  subject { ServicesGroupPolicy }
+
+  permissions ".scope" do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+
+  permissions :create? do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+
+  permissions :show? do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+
+  permissions :update? do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+
+  permissions :destroy? do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+end
